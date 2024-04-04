b8bc34e1f310e1f4e1c784eb61e2b3ead098766e
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-04-04 15:11:37 +0700

    add new func to str_func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def str_func(word):
     """Функция делает все буквы заглавными"""
     return word.upper()
+
+
+def str_func_2(word):
+    """Функция делает все слова с
+    заглавной первой буквой """
+    return word.title()
